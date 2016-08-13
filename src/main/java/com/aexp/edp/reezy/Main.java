bb85ec16d760f71f94e98e32dce86db8b7670731
Paul Philion <philion@acmerocket.com>
2016-08-13 08:05:04 -0400

    Moving servlet context config to Main instead of web.xml

--- src/main/java/com/aexp/edp/reezy/Main.java
+++ src/main/java/com/aexp/edp/reezy/Main.java
@@ -2,23 +2,27 @@ package com.aexp.edp.reezy;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.jboss.resteasy.plugins.server.servlet.ResteasyBootstrap;
 
 public class Main {
-    public static void main(String[] args) throws Exception
-    {
-            Server server = new Server(8080);
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
 
-            WebAppContext context = new WebAppContext();
+		WebAppContext context = new WebAppContext();
 
-            context.setDescriptor("src/main/webapp/WEB-INF/web.xml");
-            context.setResourceBase("src/main/webapp");
-            context.setContextPath("/");
+		context.setInitParameter("resteasy.resources", "com.aexp.edp.reezy.Resource");
+		context.setInitParameter("javax.ws.rs.Application", "com.aexp.edp.reezy.Config");
+		
+		context.setResourceBase("src/main/webapp");
+		context.setContextPath("/");
 
-            context.setParentLoaderPriority(true);            
+		context.setParentLoaderPriority(true);            
+		
+		context.addEventListener(new ResteasyBootstrap());
 
-            server.setHandler(context);
+		server.setHandler(context);
 
-            server.start();
-            server.join();
-    }
+		server.start();
+		server.join();
+	}
 }
