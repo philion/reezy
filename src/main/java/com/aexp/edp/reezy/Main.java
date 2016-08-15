ed90b6c246d6183d9cd2af0a8ec06ee34fc310ce
Paul Philion <philion@acmerocket.com>
2016-08-15 09:46:07 -0400

    Rolling back to working version

--- src/main/java/com/aexp/edp/reezy/Main.java
+++ src/main/java/com/aexp/edp/reezy/Main.java
@@ -2,27 +2,23 @@ package com.aexp.edp.reezy;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.jboss.resteasy.plugins.server.servlet.ResteasyBootstrap;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
+    public static void main(String[] args) throws Exception
+    {
+            Server server = new Server(8080);
 
-		WebAppContext context = new WebAppContext();
+            WebAppContext context = new WebAppContext();
 
-		context.setInitParameter("resteasy.resources", "com.aexp.edp.reezy.Resource");
-		context.setInitParameter("javax.ws.rs.Application", "com.aexp.edp.reezy.Config");
-		
-		context.setResourceBase("src/main/webapp");
-		context.setContextPath("/");
+            context.setDescriptor("src/main/webapp/WEB-INF/web.xml");
+            context.setResourceBase("src/main/webapp");
+            context.setContextPath("/");
 
-		context.setParentLoaderPriority(true);            
-		
-		context.addEventListener(new ResteasyBootstrap());
+            context.setParentLoaderPriority(true);            
 
-		server.setHandler(context);
+            server.setHandler(context);
 
-		server.start();
-		server.join();
-	}
+            server.start();
+            server.join();
+    }
 }
